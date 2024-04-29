e23d09443c4ecbdac1b036d2aaebe60049c7ec1d
leobelko <leobelko@gmail.com>
2024-04-29 12:49:24 +0000

    ✨ feat: about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -18,7 +18,20 @@ export default function About() {
                 <link rel="icon" href="/iconLogo.svg" sizes="any" />
             </Head>
             <main className="h-screen bg-light-white">
-                <NavBar/>
+                <div className="space-y-6">
+                    <NavBar/>
+                    <div className="grid justify-items-center space-y-8">
+                        <div className="grid justify-items-center">
+                            <h1 className="text-light-blue text-5xl font-semibold">Ajude seus colegas</h1>
+                            <h1 className="text-light-blue text-5xl font-semibold">e avalie seus professores!</h1>
+                        </div>
+                        <div className="grid justify-items-center">
+                            <h2 className="text-light-blue text-lg font-semibold">O sistema que reúne e te guia na escolha do seu professor!</h2>
+                            <h2 className="text-light-blue text-lg font-semibold">Encontre avaliações, comentários e orientações de seus colegas para decidir suas disciplinas</h2>
+                        </div>
+                        <img src="/about.png" alt="about"></img>
+                    </div>
+                </div>
             </main>
         </>
     )
