11e48a81f2afbc8192b134e5f1eeca060a03567a
Luccambc <luccamagalhaes@gmail.com>
2024-07-04 12:24:00 -0300

    ✨feat: singup page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,26 @@
-export default function Button({ buttonText, handleButtonClick, buttonWidth, buttonHeight }: { buttonText: string, handleButtonClick?: () => void, buttonWidth?: string, buttonHeight?: string }) {
-    const buttonStyle = `font-fira px-10 py-1 text-white bg-green rounded-md ${buttonWidth || 'w-52'} ${buttonHeight || 'h-7'}`;
-    return (
-        <button 
-            className={buttonStyle}
-            type="button"    
-            onClick={handleButtonClick}
-        >
-            {buttonText}
-        </button>
-    );
+export default function Button({
+  buttonText,
+  handleButtonClick,
+  buttonWidth,
+  buttonHeight,
+  buttonType,
+}: {
+  buttonText: string;
+  handleButtonClick?: () => void;
+  buttonWidth?: string;
+  buttonHeight?: string;
+  buttonType?: "button" | "submit" | "reset" | undefined;
+}) {
+  const buttonStyle = `font-fira px-10 py-1 text-white bg-green rounded-md ${
+    buttonWidth || "w-52"
+  } ${buttonHeight || "h-7"}`;
+  return (
+    <button
+      className={buttonStyle}
+      type={buttonType}
+      onClick={handleButtonClick}
+    >
+      {buttonText}
+    </button>
+  );
 }
